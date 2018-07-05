70dfd92868833eda042cf151009adfb3dac032cb
JoMingyu <city7310@naver.com>
2018-07-05 09:28:49 +0900

    [DONE] GET /student/verify/id/:id API

---
+++ controllers/account_student/signup.go
@@ -0,0 +1,23 @@
+package account_student
+
+import (
+	"DMS-Migrates-to-Go/db"
+	"net/http"
+
+	"github.com/labstack/echo"
+	"gopkg.in/mgo.v2-unstable/bson"
+)
+
+func Setup(router *echo.Router) {
+	// 학생 ID 중복체크
+	router.Add("GET", "/student/verify/id/:id", func(c echo.Context) error {
+		id := c.Param("id")
+
+		if count, _ := db.StudentAccountCol.Find(bson.M{"id": id}).Count(); count != 0 {
+			return c.String(http.StatusConflict, "")
+		} else {
+			return c.String(http.StatusOK, "")
+		}
+	})
+
+}
--- controllers/router.go
+++ controllers/router.go
@@ -1,7 +1,11 @@
 package controllers
 
-import "github.com/labstack/echo"
+import (
+	"DMS-Migrates-to-Go/controllers/account_student"
+
+	"github.com/labstack/echo"
+)
 
 func Setup(router *echo.Router) {
-
+	account_student.Setup(router)
 }
